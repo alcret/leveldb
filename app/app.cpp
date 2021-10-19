b13d2f995c392bd3e3d7c12a0cf271cb127b4c0f
alcret <alcret@163.com>
2021-10-19 10:02:03 +0800

    在app中添加测试代码

--- app/app.cpp
+++ app/app.cpp
@@ -1,13 +1,31 @@
 //
 // Created by T480S on 2021/10/19.
 //
+#include <cassert>
 #include <iostream>
 
+#include "leveldb/db.h"
+
 using namespace std;
+using namespace leveldb;
 
 int main() {
+  DB* db;
+  Options options;
+  options.create_if_missing = true;
+  Status status = DB::Open(options, "testDB", &db);
+  assert(status.ok());
 
+  status = db->Put(WriteOptions(), "KeyNameExample", "ValueExamples");
+  assert(status.ok());
+  string res;
+  status = db->Get(ReadOptions(), "KeyNameExample", &res);
+  assert(status.ok());
+
+  cout << "Get Value Is: " << res << endl;
   cout << "hello levelDB";
+
+  delete db;
 
   return 0;
 }
